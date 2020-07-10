e01f7eb2a5453bb229262c5adb40c76d9340e034
TM <TM>
2020-07-10 11:27:05 +0700

    [Florian, Mike] - increase kafka root ebs size to 60GB

--- modules/training_kafka/ec2.tf
+++ modules/training_kafka/ec2.tf
@@ -1,10 +1,14 @@
 resource "aws_instance" "kafka" {
-  ami                    = "${data.aws_ami.training_kafka.image_id}"
+  ami                    = "ami-030a4d71993f6b30b" //${data.aws_ami.training_kafka.image_id}"
   instance_type          = "${var.instance_type}"
   vpc_security_group_ids = ["${aws_security_group.kafka.id}"]
   subnet_id              = "${var.subnet_id}"
   key_name               = "${var.ec2_key_pair}"
   iam_instance_profile   = "${aws_iam_instance_profile.kafka.name}"
+
+  root_block_device {
+    volume_size = 60
+  }
 
   tags = "${merge(
     local.common_tags,
