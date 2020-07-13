66b8194af0f7449dbbaf55ae23c8c16105bb7392
TM <TM>
2020-07-13 14:37:24 +0700

    Jasmin,Laz,Mike - move state to s3, force encryption for transit in cluster to TLS, IaC open_monitoring

--- components/training_msk/main.tf
+++ components/training_msk/main.tf
@@ -1,3 +1,7 @@
+terraform {
+  backend "s3" {}
+}
+
 data "terraform_remote_state" "training-msk" {
   backend = "s3"
   config {
--- modules/training_msk/main.tf
+++ modules/training_msk/main.tf
@@ -4,8 +4,8 @@ resource "aws_security_group" "sg" {
   ingress {
     description = "everything"
     from_port   = 0
-    to_port     = 65535
-    protocol    = "all"
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["10.0.0.0/16"]
   }
 }
@@ -14,11 +14,29 @@ resource "aws_msk_cluster" "training_msk" {
   cluster_name           = "${var.deployment_identifier}"
   kafka_version          = "1.1.1"
   number_of_broker_nodes = 3
+  enhanced_monitoring = "PER_TOPIC_PER_BROKER"
+
+  encryption_info {
+    encryption_in_transit {
+      client_broker = "TLS"
+    }
+  }
 
   broker_node_group_info {
     instance_type   = "${var.instance_type}"
     ebs_volume_size = 100
     client_subnets = ["${var.client_subnets}"]
     security_groups = ["${aws_security_group.sg.id}"]
+  }
+
+  open_monitoring {
+    prometheus {
+      jmx_exporter {
+        enabled_in_broker = true
+      }
+      node_exporter {
+        enabled_in_broker = true
+      }
+    }
   }
 }
